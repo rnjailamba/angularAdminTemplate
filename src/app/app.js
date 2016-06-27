7decfe81a9ce09d55da979ea2bbb91e575a61530
rnjai <rnjailamba12@gmail.com>
2016-06-27 20:56:42 +0530

    Starter - add the project page and remove the side nav for this page specially

--- src/app/app.js
+++ src/app/app.js
@@ -21,8 +21,16 @@ angular.module('BlurAdmin', [
 angular.module('BlurAdmin')
   .run(function($rootScope, $location, $stateParams) {
     $rootScope.showSideBar = true;
-
+    if( ($location.path()).indexOf('/projects') !== -1 ){
+      console.log($location.path());
+      $rootScope.showSideBar = 0;
+    }
     $rootScope.$on('$routeChangeSuccess', function(event, current) {
+      if( ($location.path()).indexOf('/projects') !== -1 ){
+        console.log($location.path());
+        $rootScope.showSideBar = 0;
+      }
+
       // Look at $location.path()
       // If it isn't what you want, toggle showSideBar...
       // $rootScope.showSideBar = true|false;
---
+++ src/app/pages/projects/projects.module.js
@@ -0,0 +1,34 @@
+/**
+ * @author v.lugovsky
+ * created on 16.12.2015
+ */
+(function () {
+  'use strict';
+
+  angular.module('BlurAdmin.pages.projects', [])
+    .config(routeConfig);
+
+  /** @ngInject */
+  function routeConfig($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('projects', {
+          url: '/projects',
+          template : '<ui-view></ui-view>',
+          abstract: true,
+          controller: 'ProjectsPageCtrl',
+          title: 'Projects',
+
+        }).state('projects.basic', {
+          url: '/basic',
+          templateUrl: 'app/pages/projects/basic/tables.html',
+          title: 'Basic projects',
+
+        }).state('projects.smart', {
+          url: '/smart',
+          templateUrl: 'app/pages/projects/smart/tables.html',
+          title: 'Smart Projects',
+        });
+    $urlRouterProvider.when('/projects','/projects/basic');
+  }
+
+})();
