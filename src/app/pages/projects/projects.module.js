3404a832d408ecb5d9b4a048c1d208202e6416e7
rnjai <rnjailamba12@gmail.com>
2016-06-29 08:50:16 +0530

    Starter - Language selection added along with navbar responsive

--- src/app/pages/projects/projects.module.js
+++ src/app/pages/projects/projects.module.js
@@ -17,24 +17,17 @@
           abstract: true,
           controller: 'ProjectsPageCtrl',
           title: 'Projects',
-          sidebarMeta: {
-            icon: 'ion-grid',
-            order: 300,
-          },
+
         }).state('projects.basic', {
           url: '/basic',
           templateUrl: 'app/pages/projects/basic/tables.html',
           title: 'Basic projects',
-          sidebarMeta: {
-            order: 10,
-          },
+
         }).state('projects.smart', {
           url: '/smart',
           templateUrl: 'app/pages/projects/smart/tables.html',
           title: 'All Projects',
-          sidebarMeta: {
-            order: 0,
-          },
+
         });
     $urlRouterProvider.when('/projects','/projects/basic');
   }
